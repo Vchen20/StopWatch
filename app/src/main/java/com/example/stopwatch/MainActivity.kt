6d85c68f4a4631b4ac23da6e8a3c3a8202672f4b
vchen20 <chenvic111@gmail.com>
2021-09-09 09:47:01 -0700

    Finished layout, lifestyle overrided

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -4,10 +4,16 @@ import android.content.res.Configuration
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var start : Button
+    lateinit var reset : Button
+    lateinit var stopWatch : Chronometer
 
     companion object{
         val TAG = "MainActivity"
@@ -18,10 +24,46 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        Log.d(TAG, "onCreate: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause: ")
     }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
+    private fun wireWidgets() {
+        start = findViewById(R.id.button_main_start)
+        reset = findViewById(R.id.button_main_reset)
+        stopWatch = findViewById(R.id.chronometer_main_stopwatch)
+    }
+
+
+
 
 }
 
