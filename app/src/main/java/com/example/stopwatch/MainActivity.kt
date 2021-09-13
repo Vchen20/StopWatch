fec65109e9ee9174f12c5833d7e76b2247f7afe4
vchen20 <chenvic111@gmail.com>
2021-09-13 09:34:53 -0700

    all things work

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -5,6 +5,7 @@ import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.os.SystemClock
 import android.util.Log
 import android.widget.Button
 import android.widget.Chronometer
@@ -14,13 +15,40 @@ class MainActivity : AppCompatActivity() {
     lateinit var start : Button
     lateinit var reset : Button
     lateinit var stopWatch : Chronometer
-
+    var x = false
+    var time = 0
     companion object{
         val TAG = "MainActivity"
     }
+    private fun wireWidgets() {
+        start = findViewById(R.id.button_main_start)
+        reset = findViewById(R.id.button_main_reset)
+        stopWatch = findViewById(R.id.chronometer_main_stopwatch)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wireWidgets()
+        start.setOnClickListener {
+
+            if(!x)
+            {
+                stopWatch.base = SystemClock.elapsedRealtime() + time
+                stopWatch.start()
+                x = true
+            }
+            else{
+                stopWatch.stop()
+                x = false
+                time = (stopWatch.base - SystemClock.elapsedRealtime()).toInt()
+            }
+        }
+        reset.setOnClickListener {
+            stopWatch.base = SystemClock.elapsedRealtime()
+            time = 0
+        }
+
 
 
     }
@@ -28,51 +56,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         Log.d(TAG, "onCreate: ")
-        Log.e()
     }
 
     override fun onRestart() {
         super.onRestart()
         Log.d(TAG, "onRestart: ")
-        Log.e()
     }
 
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart: ")
-        Log.e()
     }
 
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume: ")
-        Log.e()
     }
 
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause: ")
-        Log.e()
     }
 
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop: ")
-        Log.e()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy: ")
-        Log.e()
     }
 
 
-    private fun wireWidgets() {
-        start = findViewById(R.id.button_main_start)
-        reset = findViewById(R.id.button_main_reset)
-        stopWatch = findViewById(R.id.chronometer_main_stopwatch)
-    }
 
 
 
